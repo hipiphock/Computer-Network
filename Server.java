4217d2a8bd97986d20fff70bf36841e442eab45b
hipiphock <hipiphock@naver.com>
2018-11-14 21:15:24 +0900

    git push origin master

--- Server.java
+++ Server.java
@@ -1,5 +1,6 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -48,8 +49,13 @@ public class Server{
             fromClient = new DataInputStream(connSocket.getInputStream());
             toClient = new DataOutputStream(connSocket.getOutputStream());
 
-        	String command = fromClient.readUTF(); 
-            String argument = fromClient.readUTF();
+            String command = fromClient.readUTF();
+            String argument;
+            try{
+                argument = fromClient.readUTF();
+            } catch(EOFException e){
+                toClient = ".";
+            }
             System.out.println(command);
             System.out.println(argument);
 
